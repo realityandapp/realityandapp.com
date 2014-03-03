cc4ec654cfc60c152ba7751d4bef3ef889056834
destinyd <destinyd_war@163.com>
2014-03-03 09:37:33 +0800

    raa page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Raa::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.precompile += %w( at.git home/* weixin/* )
 end
